83e3de50279a191393306272ceaf0c2d6ae671b0
Valerii Kovalchuk <kovvalole@gmail.com>
2019-02-19 16:34:48 +0200

    Fix using static files in storybook

--- storybook/config/webpack.config.js
+++ storybook/config/webpack.config.js
@@ -4,12 +4,4 @@ module.exports = {
   resolve: {
     modules: ['node_modules', path.resolve(__dirname, '..', '..', 'src')],
   },
-  module: {
-    rules: [
-      {
-        test: /\.(png|woff|woff2|eot|otf|ttf|svg|jpg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url-loader',
-      },
-    ],
-  },
 };
