f4f7a2a09e8a83adc0aee8abe2e2d795d5ec162c
Valerii Kovalchuk <kovvalole@gmail.com>
2019-02-26 21:40:17 +0200

    #50 Increment input

---
+++ src/common/components/inputs/IncrementField/index.js
@@ -0,0 +1,76 @@
+// @flow
+import * as React from 'react';
+import autoBind from 'react-autobind';
+
+import { Cont, StyledInput, Modifier } from './styled';
+
+type Props = {
+  initialValue: number,
+  maxValue?: number,
+  minValue?: number,
+  step: number,
+  onChange?: number => void,
+  isPercent: boolean,
+};
+
+type State = {
+  value: number,
+};
+
+const defaultProps = {
+  initialValue: 0,
+  step: 1,
+  isPercent: false,
+};
+
+class IncrementField extends React.PureComponent<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      value: props.initialValue,
+    };
+    autoBind(this);
+  }
+
+  static defaultProps = defaultProps;
+
+  decrement() {
+    const { minValue, step, onChange } = this.props;
+    const { value } = this.state;
+    const nextValue = value - step;
+    if (!minValue || nextValue >= minValue) {
+      this.setState({ value: nextValue });
+      onChange && onChange(nextValue);
+    }
+  }
+
+  increment() {
+    const { maxValue, step, onChange } = this.props;
+    const { value } = this.state;
+    const nextValue = value + step;
+    if (!maxValue || nextValue <= maxValue) {
+      this.setState({ value: nextValue });
+      onChange && onChange(nextValue);
+    }
+  }
+
+  render() {
+    const value = this.props.isPercent
+      ? `${this.state.value}%`
+      : this.state.value.toString();
+    return (
+      <Cont>
+        <Modifier onClick={this.decrement}>-</Modifier>
+        <StyledInput
+          type="text"
+          value={value}
+          disabled={true}
+          size={value.length}
+        />
+        <Modifier onClick={this.increment}>+</Modifier>
+      </Cont>
+    );
+  }
+}
+
+export default IncrementField;
---
+++ src/common/components/inputs/IncrementField/tests/index.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import renderer from 'react-test-renderer';
+
+import IncrementField from '../index';
+
+describe('IncrementField', function() {
+  it('should render normally', function() {
+    const wrapper = mount(<IncrementField />);
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  it('should match snapshot', function() {
+    const tree = renderer
+      .create(<IncrementField />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
