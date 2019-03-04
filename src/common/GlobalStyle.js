d964f29a2555d3dbb626cda848ee3757aff56f50
Valerii Kovalchuk <kovvalole@gmail.com>
2019-03-04 21:10:10 +0200

    Header component

--- src/common/GlobalStyle.js
+++ src/common/GlobalStyle.js
@@ -15,6 +15,11 @@ const GlobalStyle = createGlobalStyle`
   h1, h2, h3, h4, h5, h6 {
     margin: 0;
   }
+  
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
 `;
 
 export default GlobalStyle;
