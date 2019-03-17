cabe7306eefd4ed72727b16e28e8271242487ccb
Valerii Kovalchuk <kovvalole@gmail.com>
2019-03-17 12:13:55 +0200

    Simplify good reducer

--- src/containers/Entities/Goods/reducer.js
+++ src/containers/Entities/Goods/reducer.js
@@ -2,7 +2,7 @@
 import type { Action as StoreAction } from 'store/actions';
 import type { AddGoodAction, UpdateGoodsMapAction } from './actions';
 import { ADD_GOOD, UPDATE_GOODS_MAP } from './constants';
-import type { Good, GoodsMap } from './types';
+import type { GoodsMap } from './types';
 
 export type State = GoodsMap;
 
@@ -10,16 +10,13 @@ export const initialState = {};
 
 type Action = StoreAction<AddGoodAction | UpdateGoodsMapAction>;
 
-const addGood = (state: State, good: Good) => {
-  const newState = { ...state };
-  newState[good.id] = good;
-  return newState;
-};
-
 export default (state: State = initialState, action: Action) => {
   switch (action.type) {
     case ADD_GOOD:
-      return addGood(state, action.good);
+      return {
+        ...state,
+        [action.good.id]: action.good,
+      };
     case UPDATE_GOODS_MAP:
       return {
         ...state,
