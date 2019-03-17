88c4eb61acaddcf526c4259329bc6c8a4bd96ad5
Valerii Kovalchuk <kovvalole@gmail.com>
2019-03-17 12:17:04 +0200

    Simplify store reducer

--- src/containers/Entities/Stores/reducer.js
+++ src/containers/Entities/Stores/reducer.js
@@ -2,7 +2,7 @@
 import type { Action as StoreAction } from 'store/actions';
 import type { AddStoreAction, UpdateStoresMapAction } from './actions';
 import { ADD_STORE, UPDATE_STORES_MAP } from './constants';
-import type { Store, StoresMap } from './types';
+import type { StoresMap } from './types';
 
 export type State = StoresMap;
 
@@ -10,16 +10,13 @@ export const initialState = {};
 
 type Action = StoreAction<AddStoreAction | UpdateStoresMapAction>;
 
-const addStore = (state: State, store: Store) => {
-  const newState = { ...state };
-  newState[store.id] = store;
-  return newState;
-};
-
 export default (state: State = initialState, action: Action) => {
   switch (action.type) {
     case ADD_STORE:
-      return addStore(state, action.store);
+      return {
+        ...state,
+        [action.store.id]: action.store,
+      };
     case UPDATE_STORES_MAP:
       return {
         ...state,
